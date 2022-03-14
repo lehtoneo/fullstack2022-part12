12888a4f02296c0b4445783258d13c2d2db640e6
Ossi Lehtonen <ossi.j.lehtonen@helsinki.fi>
2022-03-14 11:14:56 +0200

    13

--- todo-app/todo-frontend/src/util/apiClient.js
+++ todo-app/todo-frontend/src/util/apiClient.js
@@ -1,7 +1,10 @@
 import axios from 'axios'
-
+const API_URL = process.env.NODE_ENV === 'production' 
+  ? process.env.REACT_APP_BACKEND_URL 
+  : "http://localhost:3000";
+console.log({ API_URL })
 const apiClient = axios.create({
-  baseURL: process.env.REACT_APP_BACKEND_URL,
+  baseURL: API_URL,
 })
 
 export default apiClient
